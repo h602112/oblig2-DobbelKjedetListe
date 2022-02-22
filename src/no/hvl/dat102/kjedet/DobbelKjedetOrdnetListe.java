56be736533c84477f9e88b0b3f2b278ffb309910
h602112 <markusnedrevold@live.no>
2022-02-22 15:12:35 +0100

    init commit

---
+++ src/no/hvl/dat102/kjedet/DobbelKjedetOrdnetListe.java
@@ -0,0 +1,125 @@
+package no.hvl.dat102.kjedet;
+
+import no.hvl.dat102.adt.DobbelKjedetOrdnetListeADT;
+import no.hvl.dat102.exceptions.EmptyCollectionException;
+
+public class DobbelKjedetOrdnetListe<T extends Comparable<T>> implements DobbelKjedetOrdnetListeADT<T> {
+	private DobbelNode<T> foerste;
+	private DobbelNode<T> siste;
+	private int antall;
+
+	public DobbelKjedetOrdnetListe(T minVerdi, T maksVerdi) {
+		// Første node
+		foerste = null;
+		//TODO
+		// Siste node
+		siste = null;
+		//TODO
+		// Kjeding
+		antall = 0;
+		//TODO
+	}
+
+	@Override
+	public void leggTil(T el) {
+		DobbelNode<T> nyNode = new DobbelNode<T>(el);
+		DobbelNode<T> aktuell = foerste.getNeste();
+		while ((el.compareTo(aktuell.getElement()) > 0)) {
+			aktuell = aktuell.getNeste();
+		}
+		// Legg inn foran aktuell
+		nyNode.setNeste(aktuell);
+		nyNode.setForrige(aktuell.getForrige());
+		aktuell.getForrige().setNeste(nyNode);
+		aktuell.setForrige(nyNode);
+		antall++;
+
+	}
+	public boolean fins(T el) {
+		boolean found = false;
+		DobbelNode<T> current = foerste;
+		if (foerste == null) {
+			found = false;
+		}
+		while (current != null) {
+			if (current.getElement() == el) {
+				found = true;
+				break;
+			}
+			current = current.getNeste();
+		}
+		return found;
+
+	}
+	public void visListe() {
+		DobbelNode<T> current = foerste;
+		while (current != null) {
+			System.out.println(current.getElement());
+			current = current.getNeste();
+		}
+
+	}
+
+	@Override
+	public T fjern(T el) {
+		T resultat = null;
+		if (erTom())
+			throw new EmptyCollectionException("dobbelkjedet ordnet liste er tom");
+		DobbelNode<T> aktuell = finn(el);
+		if (aktuell != null) {// returner og slett
+			resultat = aktuell.getElement();
+			aktuell.getForrige().setNeste(aktuell.getNeste());
+			aktuell.getNeste().setForrige(aktuell.getForrige());
+
+		}
+
+		return resultat;
+
+	}
+
+	/*
+	 * Returnerer referansen til noden hvis el fins, ellers returneres
+	 * null-referansen
+	 */
+	private DobbelNode<T> finn(T el) {
+		return null;
+		//TODO
+
+	}
+
+	@Override
+	public boolean erTom() {
+		return (antall == 0);
+	}
+
+	@Override
+	public int antall() {
+		return antall;
+	}
+
+	public String toString() {
+		String resultat = "";
+		DobbelNode<T> aktuell = foerste.getNeste();
+		while (aktuell != siste) {
+
+			resultat = resultat + aktuell.getElement().toString();
+			aktuell = aktuell.getNeste();
+		}
+
+		return resultat;
+	}
+
+	public String tilStrengBaklengs() {
+		String resultat = "";
+		DobbelNode<T> aktuell = siste.getForrige();
+		while (aktuell != foerste) {
+
+			resultat = resultat + aktuell.getElement().toString();
+			aktuell = aktuell.getForrige();
+		}
+
+		return resultat;
+
+	}
+
+}
